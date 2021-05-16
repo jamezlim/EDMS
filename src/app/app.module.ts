d2e1766f0a37a079bef4d1fc6524c159df9af1cb
James Lim <limcheolhyun930421@gmail.com>
2021-05-16 12:39:36 -0400

    added tool bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PatientModule } from './patient/patient.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { PatientModule } from './patient/patient.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // angular core library 
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+
+    //ng-material
+    MatButtonModule,
+    MatTooltipModule,
+    MatToolbarModule,
+
     PatientModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
