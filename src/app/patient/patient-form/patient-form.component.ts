95e1cd57619986062b5de8e59749e8c702bb7e4b
James Lim <limcheolhyun930421@gmail.com>
2021-05-27 01:36:02 -0400

    added css to forms

--- src/app/patient/patient-form/patient-form.component.ts
+++ src/app/patient/patient-form/patient-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import {MatAccordion} from '@angular/material/expansion';
+
 
 @Component({
   selector: 'app-patient-form',
@@ -6,11 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-form.component.css']
 })
 export class PatientFormComponent implements OnInit {
-
-  constructor() { }
+  constructor(){}
+  Today : Date;
+  isOpen : boolean;
+  @ViewChild(MatAccordion) accordion: MatAccordion;
 
   ngOnInit(): void {
+    this.isOpen = true;
     var title = "Patient Form";
+    // this.Today = new Date();
   }
 
+
+  patientInfoForm = new FormGroup({ 
+    Name : new FormControl(''), 
+    Gender : new FormControl(''),
+    DOB : new FormControl(''),
+    Address : new FormControl(''),
+    Cell : new FormControl(''),
+    Home : new FormControl(''),
+    Email : new FormControl(''),
+    Insurance : new FormControl(''),
+    InsuranceId : new FormControl(''),
+    CompanyName : new FormControl(''),
+  });
 }
--- src/app/patient/patient.module.ts
+++ src/app/patient/patient.module.ts
@@ -1,13 +1,28 @@
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientFormComponent } from './patient-form/patient-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+
 
 
 
 @NgModule({
   declarations: [PatientFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatButtonModule
+
+
   ]
 })
 export class PatientModule { }
